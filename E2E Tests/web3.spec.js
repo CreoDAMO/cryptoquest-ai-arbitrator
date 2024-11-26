9e8bd15500f07182bc4898aaaab67df9040f9734
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-26 11:02:26 -0500

    Update web3.spec.js

--- E2E Tests/web3.spec.js
+++ E2E Tests/web3.spec.js
@@ -1,13 +1,7 @@
-describe('Web3 Mock Tests', () => {
-  before(() => {
-    // Stub Web3 data calls
-    cy.stub(ethers.Contract.prototype, 'totalStaked').resolves(ethers.utils.parseEther('1000'));
-    cy.stub(ethers.Contract.prototype, 'getAPY').resolves(12.5);
-  });
-
-  it('Displays staking data correctly', () => {
-    cy.visit('/ecosystem');
-    cy.get('#totalStaked').should('contain.text', '1000');
-    cy.get('#apy').should('contain.text', '12.5%');
+describe('Web3 Integration', () => {
+  it('Detects chain mismatch', () => {
+    cy.stub(window.ethereum, 'request').resolves('wrong-chain');
+    cy.visit('/');
+    cy.contains('Please switch to the correct network').should('be.visible');
   });
 });
