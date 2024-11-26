2236ac2098c287a4751247453ecf1ae0e6533a4d
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-26 10:40:34 -0500

    Create TradingVolumeChart.js

---
+++ src/components/TradingVolumeChart.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+const TradingVolumeChart = ({ data }) => {
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+    svg.selectAll('*').remove();
+
+    const width = 400;
+    const height = 200;
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+
+    const x = d3
+      .scaleBand()
+      .domain(data.map((d) => d.time))
+      .range([margin.left, width - margin.right])
+      .padding(0.1);
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.volume)])
+      .nice()
+      .range([height - margin.bottom, margin.top]);
+
+    svg
+      .append('g')
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+
+    svg
+      .selectAll('.bar')
+      .data(data)
+      .join('rect')
+      .attr('class', 'bar')
+      .attr('x', (d) => x(d.time))
+      .attr('y', (d) => y(d.volume))
+      .attr('height', (d) => y(0) - y(d.volume))
+      .attr('width', x.bandwidth())
+      .attr('fill', 'steelblue');
+  }, [data]);
+
+  return <svg ref={svgRef} width={400} height={200}></svg>;
+};
+
+export default TradingVolumeChart;
