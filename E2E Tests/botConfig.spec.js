93ce4a9485390bed32551cdd373869e6359dad99
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-26 10:52:04 -0500

    Create botConfig.spec.js

---
+++ E2E Tests/botConfig.spec.js
@@ -0,0 +1,10 @@
+describe('Bot Configuration Tests', () => {
+  it('Validates required fields', () => {
+    cy.visit('/bot-configuration');
+    cy.contains('Start Bot').click();
+    cy.contains('Trading Strategy is required').should('be.visible');
+  });
+
+  it('Submits bot configuration successfully', () => {
+    cy.visit('/bot-configuration');
+    cy.get('select[name="
