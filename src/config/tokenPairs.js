be8e0dd5afdbc0b526cf218861dacb35ab8c9237
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-26 10:28:27 -0500

    Create tokenPairs.js

---
+++ src/config/tokenPairs.js
@@ -0,0 +1,15 @@
+const TOKEN_PAIRS = {
+  MATIC_CQT: {
+    address: process.env.REACT_APP_MATIC_CQT_PAIR_ADDRESS,
+    label: 'MATIC/CQT',
+  },
+  WBTC_CQT: {
+    address: process.env.REACT_APP_WBTC_CQT_PAIR_ADDRESS,
+    label: 'WBTC/CQT',
+  },
+  WETH_CQT: {
+    address: process.env.REACT_APP_WETH_CQT_PAIR_ADDRESS,
+    label: 'WETH/CQT',
+  },
+};
+export default TOKEN_PAIRS;
