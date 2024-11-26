0418bd396dc080e2b7d571561ebfa029afc2c008
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-26 15:08:48 -0500

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,25 @@
-const FlatESLintConfig = require('eslint').FlatESLintConfig;
-
-module.exports = new FlatESLintConfig({
-  ignores: ['node_modules', 'build', '.next'],
-  files: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-  },
-  settings: {
-    react: {
-      version: 'detect',
+export default [
+  {
+    ignores: ['node_modules', 'build', '.next'],
+    files: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react: require('eslint-plugin-react'),
     },
   },
-  plugins: {
-    react: require('eslint-plugin-react'),
-  },
-});
+];
 
