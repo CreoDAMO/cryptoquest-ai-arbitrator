deee739b8dd76e1a3a76468bfc5f994876bae7fa
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-26 14:46:24 -0500

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import { FlatESLintConfig } from 'eslint';
+
+export default new FlatESLintConfig({
+  ignores: ['node_modules', 'build', '.next'],
+  files: ['src/**/*.js', 'src/**/*.jsx', 'src/**/*.ts', 'src/**/*.tsx'],
+  languageOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // Example rules
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: {
+    react: require('eslint-plugin-react'),
+  },
+});
