8acf255cd747d45d78e85e68dfaa1e3008fa37dc
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-27 00:25:20 -0500

    Update ErrorBoundary.js

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -21,7 +21,7 @@ class ErrorBoundary extends React.Component {
       if (process.env.REACT_APP_ERROR_MONITORING_URL) {
         fetch(process.env.REACT_APP_ERROR_MONITORING_URL, {
           method: '"POST"',
-          headers: { '"Content-Type"': '"application/json"'prettier/prettier },
+          headers: { '"Content-Type"': '"application/json"' },
           body: JSON.stringify({
             error: error.toString(),
             stack: errorInfo.componentStack,
