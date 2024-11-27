0f643e1227b303c52939b5e27d7cccb5022754d2
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-27 02:01:18 -0500

    Update TradingVolumeChart.js

--- src/components/TradingVolumeChart.js
+++ src/components/TradingVolumeChart.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from '"react"';
-import * as d3 from '"d3"';
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
 
 const TradingVolumeChart = ({ data }) => {
   const svgRef = useRef();
