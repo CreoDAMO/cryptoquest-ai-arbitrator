c13b33304f1106812dd30985d87acbc1f1ea8951
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-27 02:10:59 -0500

    Update app.spec.js

--- E2E Tests/app.spec.js
+++ E2E Tests/app.spec.js
@@ -1,13 +1,13 @@
-describe('"CryptoQuest AI Arbitrator"', () => {
-  it('"Loads the homepage"', () => {
+describe("CryptoQuest AI Arbitrator", () => {
+  it("Loads the homepage", () => {
     cy.visit('/');
-    cy.contains('CryptoQuest AI Arbitrator').should('be.visible');
+    cy.contains("CryptoQuest AI Arbitrator").should("be.visible");
   });
 
-  it('"Toggles the theme"', () => {
-    cy.get('"#themeToggle"').click();
-    cy.get('"body"').should('"have.class"', '"light-theme"');
-    cy.get('"#themeToggle"').click();
-    cy.get('"body"').should('"not.have.class"', '"light-theme"');
+  it("Toggles the theme", () => {
+    cy.get("#themeToggle").click();
+    cy.get("body").should("have.class", "light-theme");
+    cy.get("#themeToggle").click();
+    cy.get("body").should("not.have.class", "light-theme");
   });
 });
