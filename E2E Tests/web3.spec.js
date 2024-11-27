3454a3624d7fbed11c998a9f16232c7b5fd5ee43
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-27 02:19:36 -0500

    Update web3.spec.js

--- E2E Tests/web3.spec.js
+++ E2E Tests/web3.spec.js
@@ -1,7 +1,7 @@
-describe('Web3 Integration', () => {
-  it('Detects chain mismatch', () => {
-    cy.stub(window.ethereum, 'request').resolves('wrong-chain');
-    cy.visit('/');
-    cy.contains('Please switch to the correct network').should('be.visible');
+describe("Web3 Integration", () => {
+  it("Detects chain mismatch", () => {
+    cy.stub(window.ethereum, "request").resolves("wrong-chain");
+    cy.visit("/');
+    cy.contains("Please switch to the correct network").should("be.visible");
   });
 });
