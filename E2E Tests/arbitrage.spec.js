3f45ea6a1f07e799d207e2ce6316d578c23f6754
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-27 02:11:33 -0500

    Update arbitrage.spec.js

--- E2E Tests/arbitrage.spec.js
+++ E2E Tests/arbitrage.spec.js
@@ -1,5 +1,5 @@
-describe('TradingFlowVisualization Component', () => {
-  it('Renders trades correctly', () => {
+describe("TradingFlowVisualization Component", () => {
+  it("Renders trades correctly", () => {
     const mockTrades = [
       { source: { position: [0, 0, 0] }, destination: { position: [1, 1, 1] }, volume: 100 },
       { source: { position: [2, 2, 2] }, destination: { position: [3, 3, 3] }, volume: 50 },
