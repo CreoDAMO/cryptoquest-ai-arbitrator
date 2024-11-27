cccb58704e719ff0826b527da34891aeeaf28a52
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-11-27 01:07:12 -0500

    Update TradingVolumeChart.js

--- src/components/TradingVolumeChart.js
+++ src/components/TradingVolumeChart.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef } from '"react"';
-import * as d3 from '"d3"';
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
 
 const TradingVolumeChart = ({ data }) => {
   const svgRef = useRef();
 
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-    svg.selectAll('"*"').remove();
+    svg.selectAll("*").remove();
 
     const width = 400;
     const height = 200;
@@ -25,25 +25,25 @@ const TradingVolumeChart = ({ data }) => {
       .range([height - margin.bottom, margin.top]);
 
     svg
-      .append('"g"')
-      .attr('"transform"', `translate(0,${height - margin.bottom})`)`
+      .append("g")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x).tickSizeOuter(0));
 
     svg
-      .append('"g"')
-      .attr('"transform"', `translate(${margin.left},0)`)`
+      .append("g")
+      .attr("transform", `translate(${margin.left},0)`)
       .call(d3.axisLeft(y));
 
     svg
-      .selectAll('".bar"')
-      .data('"data"')
-      .join('"rect"')
-      .attr('"class"', '"bar"')
-      .attr('"x"', (d) => x(d.time))
-      .attr('"y"', (d) => y(d.volume))
-      .attr('"height"', (d) => y(0) - y(d.volume))
-      .attr('"width"', x.bandwidth())
-      .attr('"fill"', '"steelblue"');
+      .selectAll(".bar")
+      .data(data)
+      .join("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => x(d.time))
+      .attr("y", (d) => y(d.volume))
+      .attr("height", (d) => y(0) - y(d.volume))
+      .attr("width", x.bandwidth())
+      .attr("fill", "steelblue");
   }, [data]);
 
   return <svg ref={svgRef} width={400} height={200}></svg>;
